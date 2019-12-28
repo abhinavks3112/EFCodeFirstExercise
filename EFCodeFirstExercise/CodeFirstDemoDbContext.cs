08ad89124599ec7eea20e0610f85e5e293c1937f
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-12-28 19:18:07 +0530

    Created Video and Genre entities with many to many relationship between the two, created migration and updated the database

---
+++ EFCodeFirstExercise/CodeFirstDemoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstExercise
+{
+    public class CodeFirstDemoDbContext : DbContext
+    {
+        public CodeFirstDemoDbContext()
+        {
+
+        }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
