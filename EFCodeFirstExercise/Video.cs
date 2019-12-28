eecaf75b361cbc907400f3bca1154b56c8315807
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-12-28 19:47:02 +0530

    Modified Video entity model such that each Video has only one Genre, created migration for the same and updated the database

--- EFCodeFirstExercise/Video.cs
+++ EFCodeFirstExercise/Video.cs
@@ -11,7 +11,6 @@ namespace EFCodeFirstExercise
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
-
-        public ICollection<Genre> Genres { get; set; }
+        public Genre Genre { get; set; }
     }
 }
