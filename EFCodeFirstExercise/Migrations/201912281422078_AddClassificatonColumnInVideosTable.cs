69c1770a2e42ce7020cb45b9d1f60edb470fae26
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-12-28 19:56:27 +0530

    Created enum Classification and added it as a property in Video entity model, created migration for the same and updated the database

---
+++ EFCodeFirstExercise/Migrations/201912281422078_AddClassificatonColumnInVideosTable.cs
@@ -0,0 +1,18 @@
+﻿namespace EFCodeFirstExercise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddClassificatonColumnInVideosTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Videos", "Classification", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Videos", "Classification");
+        }
+    }
+}
