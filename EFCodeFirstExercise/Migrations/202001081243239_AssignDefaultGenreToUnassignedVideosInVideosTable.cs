4325926de896360bb0334596fb903845157fe561
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2020-01-08 20:43:24 +0530

    Populated records in Videos table with default genre 1

---
+++ EFCodeFirstExercise/Migrations/202001081243239_AssignDefaultGenreToUnassignedVideosInVideosTable.cs
@@ -0,0 +1,18 @@
+﻿namespace EFCodeFirstExercise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AssignDefaultGenreToUnassignedVideosInVideosTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE Videos SET Genre_Id = 1 WHERE Genre_Id IS NULL");
+        }
+        
+        public override void Down()
+        {
+            Sql("UPDATE Videos SET Genre_Id = NULL WHERE Genre_Id = 1");
+        }
+    }
+}
